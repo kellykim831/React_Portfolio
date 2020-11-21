968d01ebbd27924eaa8a4e664dce4e77ccf4c6b7
Kelly Kim <kellykim408@gmail.com>
2020-11-21 15:51:11 -0800

    update route path to React_Portfolio

--- portfolioapp/src/App.js
+++ portfolioapp/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path="/about">
           <About></About>
         </Route>
-        <Route path="/" exact>
+        <Route path="/React_Portfolio" exact>
           <Landing></Landing>
         </Route>
       </Switch>
